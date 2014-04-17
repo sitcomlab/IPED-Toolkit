6c3860fb4001aeb283c631336591542c2d7c45fa
Nicho <n.schiestel@uni-muenster.de>
2014-04-17 17:06:17 +0200

    Rest-API-Structure
    - tested simple Rest-API-Structure
    - Changed Port to: 8080

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var app = express();
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api-url', function(req, res){
-	res.send('Hallohallo');
+app.get('/api-url/:id', function(req, res){
+	res.send('Hello World' + req.params.id);
+	console.log(req.params.id);
 });
 
-app.listen(8081);
+app.listen(8080);
 console.log("App listens on http://localhost:8080");
