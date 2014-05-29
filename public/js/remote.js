a42f9ae3c6d3a3023b5247a07ef165f95bcb7524
TobiTobsen92 <tobias.brueggentisch@web.de>
2014-05-29 17:10:06 +0200

    testing with nicho

--- public/js/remote.js
+++ public/js/remote.js
@@ -133,7 +133,7 @@ function loadDescription(id) {
 
 	//$("#title").empty();
 	//$("#description").empty();
-	
+
 	//Fill video description with source
 	$("#video-info h4 #title").text(video.name);
 	console.log('gps: ' + video.gps);
