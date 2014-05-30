2941250c9e9ed6f4a1404f2263fc81e37dcba9e6
Morin Ostkamp <morin.ostkamp@uni-muenster.de>
2014-05-30 10:55:54 +0200

    Fixed wrong size of HTML overlays.

--- public/js/iped.js
+++ public/js/iped.js
@@ -6,9 +6,9 @@
 
 
 // Choose suitable one
-//var SERVER_URL = "http://giv-sitcomlab.uni-muenster.de";
+var SERVER_URL = "http://giv-sitcomlab.uni-muenster.de";
 // Production environment
-var SERVER_URL = "http://localhost"; // Developer environment
+//var SERVER_URL = "http://localhost"; // Developer environment
 var PORT = ":8080/";
 var currentId;
 var video_height;
