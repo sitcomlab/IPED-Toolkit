b9cd8ac8b36381240e0a531c57f00cf7f56647d3
TobiTobsen92 <tobias.brueggentisch@web.de>
2014-06-18 19:31:02 +0200

    Added backend.js in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function(grunt) {
 		'public/js/webRTC.js': 'src/frontend/webRTC.js',
 		// Backend
 		'public/backend/index.html': 'src/backend/index.html',
-		'public/backend/map.html': 'src/backend/map.html'
+		'public/backend/map.html': 'src/backend/map.html',
+		'public/js/backend.js': 'src/backend/backend.js'
 	};
 	
   // Project configuration.
