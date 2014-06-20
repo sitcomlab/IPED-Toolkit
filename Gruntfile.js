f702b10308d4f7b1e6873e9ef520de0c46ee2592
TobiTobsen92 <tobias.brueggentisch@web.de>
2014-06-20 16:13:56 +0200

    updated gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,6 +18,7 @@ module.exports = function(grunt) {
 		// Backend
 		'public/backend/index.html': 'src/backend/index.html',
 		'public/backend/map.html': 'src/backend/map.html',
+		'public/backend/addLocationDialog.html' : 'src/backend/addLocationDialog.html',
 		'public/js/backend.js': 'src/backend/backend.js',
 		'public/js/leaflet.contextmenu.js': 'src/backend/leaflet.contextmenu.js'
 	};
