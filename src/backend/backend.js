501891768eb1b139a0b3b82c05fc86aa5bc09400
TobiTobsen92 <tobias.brueggentisch@web.de>
2014-07-04 10:44:08 +0200

    fixed submitting of locations

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -4,6 +4,7 @@
  */
 var glob = [];
 var map; 
+var coords;
 
 /*	++++++++++++++++++++++
  *	EVENT LISTENERS 
@@ -60,17 +61,14 @@ $(document).ready(function(){
     	maxZoom: 18
 	}).addTo(map);
 	drawMarkers();
-	
 	map.on('contextmenu', function(e) {
-	var coords = e.latlng;
+	coords = e.latlng;
 	$("#coordinates").text(coords.lat + ", " + coords.lng);
     console.log("Coordinates saved: " + coords); // e is an event object (MouseEvent in this case)
     console.log("Latitude: " + coords.lat + ", Longitude: " + coords.lng);
 	});
 	
-	console.log("Test of map.on.coords: " + map.on.coords);
 });
-
 
 
 /*	++++++++++++++++++++++
@@ -127,6 +125,7 @@ var router = new ROUTER();
 //Router events
 router.on('route:home', function(){
 	console.log("Route: home");
+	drawMarkers();
 });
 
 router.on('route:addLocation', function(){
@@ -183,23 +182,34 @@ function submitLocation(){
 		locdescription = $("#description").val();
 	var loctags = [],	
 		loctags = "[" + $("#tags").val() + "]";
-		
+	var latitude = coords.lat,
+		longitude = coords.lng;
+			
 		console.log("Location values: " + locname + ", " + locdescription + ", " + loctags);
 		
-	var newLocation = new LocationModel;	
+	var newLocation = new LocationModel();	
 	var locationDetails = {
 		name: locname,
 		description: locdescription,
-		tags: loctags
+		tags: loctags,
+		lat: latitude,
+		lon: longitude
 	};	
 	
+	console.log("Object to be transmitted: ");
+	console.log(locationDetails);
 	newLocation.save(locationDetails, {
 		headers: {"contentType": "application/json", "dataType": "json"},
 		success: function(newLocation){
 			console.log("New Location was submitted:");
 			console.log(newLocation.toJSON());
+			router.navigate('', {trigger: true});
+			$("#add-location-dialog").dialog("close");
+			
 		}
 	});
+	
+	
 	
 		
 	
