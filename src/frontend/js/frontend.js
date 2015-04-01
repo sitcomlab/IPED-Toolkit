00a49a5994cde5b230f84091e80570c9e6f88516
Nicho S <n.schiestel@uni-muenster.de>
2015-04-01 06:19:30 +0200

    Updating Frontend

--- src/frontend/js/frontend.js
+++ src/frontend/js/frontend.js
@@ -4,6 +4,10 @@
  *
  * (c) 2014 Morin Ostkamp, Tobias Brüggentisch, Nicholas Schiestel
  * Institute for Geoinformatics (ifgi), University of Münster
+ *
+ * Voice control
+ * (c) 2015 Nicholas Schiestel
+ * Institute for Geoinformatics (ifgi), University of Münster
  */
 
 require.config(requireConfig);
@@ -23,12 +27,16 @@ require(['jsnlog/js/jsnlog.min',
 
         // Additional iPED Toolkit Plugins, e.g., Overlays
         'frontend/overlayPlugin',
-        'frontend/chromaKeyPlugin'
+        'frontend/chromaKeyPlugin',
+
+        // Additinal Plugins for Voice Control
+        'microphone/microphone'
+
     ],
 
     function(JSNLog, JQuery, io, getUrlParameters, Underscore, Backbone,
         Location, Locations, Video, Videos,
-        OverlayPlugin, ChromaKeyPlugin) {
+        OverlayPlugin, ChromaKeyPlugin, Microphone) {
         (function setupJSNLog() {
             var consoleAppender = JL.createConsoleAppender('consoleAppender');
             JL()
@@ -44,11 +52,29 @@ require(['jsnlog/js/jsnlog.min',
              */
         })();
 
+
+        /**
+         * Global variables for saving the current and previous LocationID for Voice Control Commands
+         * and for logging the times
+         */
+        var currentLocation = null;
+        var previousLocation = null;
+        var currTimestamp = null;
+        var micStart = null;
+        var micStop = null;
+
+        /**
+         * Global variables for micPermission and language if Frontend or Remote Control App will be refreshed
+         */
+        var micPermission = null;
+        var language = null;
+
         /**
          * The frontend of the iPED Toolkit.
          * @constructor
          */
         function Frontend() {
+
             this.location = null;
             this.socket = null;
             this.video = new Video;
@@ -61,7 +87,72 @@ require(['jsnlog/js/jsnlog.min',
 
             _.bindAll(this, 'onWindowResize');
             window.addEventListener('resize', this.onWindowResize);
+
+            micPermission = 0;
+            language = null;
+
+            /**
+             * Microphone Initialisation
+             * from Wit.Ai WebSDK
+             */
+            this.mic = new Wit.Microphone();
+
+            this.mic.onready = function() {
+                console.info("Microphone is ready to record");
+
+                this.socket = io();
+                micPermission = 1;
+                JL('iPED Toolkit.Frontend setRemoteMicPermission to')
+                    .info(micPermission);
+                this.socket.emit('setRemoteMicPermission', micPermission);
+
+            };
+            this.mic.onaudiostart = function() {
+                console.info("Recording started");
+            };
+            this.mic.onaudioend = function() {
+                console.info("Recording stopped, processing started");
+            };
+            this.mic.onresult = function(intent, entities, res) {
+
+                var witOnResult = new Date()
+                    .getTime();
+
+                // add LocationID for Neo4J checking
+                if (currentLocation == null) {
+                    res.locationID = null;
+                } else {
+                    res.locationID = currentLocation;
+                    res.previousLocationID = previousLocation;
+                }
+
+                // add logging times
+                res.currTimestamp = currTimestamp;
+                res.micStart = micStart;
+                res.micStop = micStop;
+                res.witOnResult = witOnResult;
+
+                this.socket = io();
+                this.socket.emit('witResponse', res);
+            };
+            this.mic.onerror = function(err) {
+                console.error("Error: " + err);
+            };
+            this.mic.onconnecting = function() {
+                console.info("Microphone is connecting");
+            };
+            this.mic.ondisconnected = function() {
+                console.info("Microphone is not connected");
+            };
+
+            function kv(k, v) {
+                if (toString.call(v) !== "[object String]") {
+                    v = JSON.stringify(v);
+                }
+                return k + "=" + v + "\n";
+            }
         }
+
 
         /**
          * Activates the web sockets that are used by the remote control.
@@ -69,13 +160,147 @@ require(['jsnlog/js/jsnlog.min',
         Frontend.prototype.activateWebSockets = function() {
             var thiz = this;
             this.socket = io();
-            this.socket.on('setLocationId', function(data) {
-                JL('iPED Toolkit.Frontend')
-                    .debug(data);
-                thiz.setLocationId(data);
-            });
+
+
             JL('iPED Toolkit.Frontend')
                 .debug('Web sockets activated');
+            this.micPermission = 0;
+            this.socket.emit('resetFrontendMicPermission', this.micPermission);
+            JL('iPED Toolkit.Frontend resetFrontendMicPermission to')
+                .info(this.micPermission);
+
+
+            /*
+                Set new LocationID for changing a video
+             */
+            this.socket.on('setLocationId', function(data) {
+
+                if (typeof data != "number") {
+                    thiz.setLocationId(data.id);
+
+                    // Logger
+                    data.frontendRecieved = new Date()
+                        .getTime();
+                    socket.emit('beforeMainLogger', data);
+                } else {
+                    thiz.setLocationId(data);
+                }
+
+                JL('iPED Toolkit.Frontend')
+                    .debug(data);
+
+
+            });
+
+            /*
+                Set MicPermission for Remote Control App
+             */
+            this.socket.on('getMicPermission', function(data) {
+                socket.emit('setRemoteMicPermission', micPermission);
+                JL('iPED Toolkit.Frontend setRemoteMicPermission to')
+                    .debug(micPermission);
+            });
+
+            /*
+                Set selected language for Remote Control App
+             */
+            this.socket.on('getSelectedLanguage', function(data) {
+                socket.emit('setSeletedLanguage', language);
+                JL('iPED Toolkit.Frontend setSeletedLanguage to')
+                    .debug(language);
+            });
+
+
+            /*
+                Setup Microphone with selected language and its corresponing Wit.Ai instance (CLIENT_ACCESS_TOKEN)
+             */
+            this.socket.on('setupMic', function(data) {
+                JL('iPED Toolkit.Frontend')
+                    .debug(data);
+
+                language = data;
+
+                var CLIENT_ACCESS_TOKEN;
+                if (data == 'en') {
+                    CLIENT_ACCESS_TOKEN = "RSVCNOQAOK4YZAAJXUK7LJYRVRGIU7LU";
+                } else if ('de') {
+                    CLIENT_ACCESS_TOKEN = "LBR7ZCQBU7Y5TAW5QR4GRWLF6KDQZSQD";
+                }
+
+                if (CLIENT_ACCESS_TOKEN != null || CLIENT_ACCESS_TOKEN != undefined) {
+                    thiz.mic.connect(CLIENT_ACCESS_TOKEN);
+
+                } else {
+                    alert("Could not access to Wit.Ai!");
+                }
+
+
+
+            });
+
+
+            /*
+                Start or Stop recording with the microphone, depends on the users input in the remote control app
+             */
+            this.socket.on('listenMic', function(data) {
+                JL('iPED Toolkit.Frontend')
+                    .debug(data);
+
+                if (data == 1) {
+                    JL('iPED Toolkit.Frontend')
+                        .debug('activate Microphone and start recording');
+
+                    // Play activating sound for user
+                    ion.sound.play("mic_start");
+
+                    // Turn the volume of the current video down, because of better voice recording
+                    $('#iPED-Video')[0].volume = 0.1;
+
+                    // Start microphone recording with a delay, because of playing the activating sound
+                    var delay = setTimeout(function() {
+
+                        thiz.mic.start();
+
+                        // Logger
+                        currTimestamp = new Date();
+                        micStart = new Date()
+                            .getTime();
+
+                    }, 460);
+
+
+                } else if (data == 0) {
+                    JL('iPED Toolkit.Frontend')
+                        .debug('deactivate Microphone and stop recording');
+
+                    // Play deactivating sound for user
+                    ion.sound.play("mic_stop");
+
+                    // Stop microphone recording
+                    thiz.mic.stop();
+
+                    // Logger
+                    micStop = new Date()
+                        .getTime();
+
+                    // Turn the volume of the current video down, because of better voice recording
+                    $('#iPED-Video')[0].volume = 1.0;
+                } else {
+                    alert("Something went wrong in the Remote Control App!");
+                }
+            });
+
+            /*
+                Inform user, if system failed, e.g. nothing found in Database or if an empty user input occors
+             */
+            this.socket.on('failed', function(data) {
+                JL('iPED Toolkit.Remote - Voice Command failed')
+                    .error(data);
+
+                // Play failure sound for user
+                ion.sound.play("voice_command_failed");
+            });
+
         };
 
         /**
@@ -83,6 +308,7 @@ require(['jsnlog/js/jsnlog.min',
          * @param {Number} locationId - The ID of the requested location
          */
         Frontend.prototype.setLocationId = function(locationId) {
+
             var thiz = this;
 
             if (!locationId) {
@@ -93,6 +319,16 @@ require(['jsnlog/js/jsnlog.min',
 
             $('.messages')
                 .empty();
+
+
+            // Save previous and current LocationID global for Voice Control
+            if (previousLocation == null && currentLocation == null) {
+                currentLocation = locationId;
+            } else {
+                previousLocation = currentLocation;
+                currentLocation = locationId;
+            }
+
 
             JL('iPED Toolkit.Frontend')
                 .info('Set location ID to: ' + locationId);
@@ -186,6 +422,20 @@ require(['jsnlog/js/jsnlog.min',
                 });
                 var chromaKeyPlugin = new ChromaKeyPlugin({
                     parent: overlayPlugin
+                });
+
+                // Load files for Voice Control Sounds
+                ion.sound({
+                    sounds: [{
+                        name: "mic_start"
+                    }, {
+                        name: "mic_stop"
+                    }, {
+                        name: "voice_command_failed"
+                    }],
+                    volume: 1.0,
+                    path: "sounds/",
+                    preload: true
                 });
             });
     }
